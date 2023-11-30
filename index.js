89643a91d6e7e264f19c8f73942bc2ae43b9eb4f
kugurerdem <k.ugurerdem@gmail.com>
2023-12-01 02:52:32 +0300

    Implement game logic and player movement

---
+++ index.js
@@ -0,0 +1,100 @@
+(async () => {
+
+    const
+        {fromPairs, invert, range, flatMap} = _,
+        {SpriteSheet, loadImage} = utils,
+
+        canvas = document.getElementById('canvas'),
+        ctx = canvas.getContext('2d'),
+
+        spriteSheet = await loadImage('assets/characters/player.png').then((sheetImg) => {
+            return new SpriteSheet({
+                image: sheetImg,
+                frames: fromPairs(range(0, 6).flatMap((i) => ([
+                    [`idle-{i}`, {x: i * 48, y: 0, w: 48, h: 48}],
+                    [`walk-right-${i}`, {x: i * 48, y: 4 * 48, w: 48, h: 48}],
+                    [`walk-left-${i}`, {
+                        x: i * 48, y: 4 * 48, w: 48, h: 48, flip: true,
+                    }],
+                    [`walk-up-${i}`, {x: i * 48, y: 5 * 48, w: 48, h: 48}],
+                    [`walk-down-${i}`, {x: i * 48, y: 3 * 48, w: 48, h: 48}],
+                ]))),
+                scale: 2,
+            })
+        }),
+
+        player = {
+            spriteSheet,
+
+            x: 0,
+            y: 0,
+
+            xVel: 0,
+            yVel: 0,
+
+            handleInput: function (input, keysState) {
+                this.yVel = (
+                    (keysState['ArrowUp'] ? -50 : 0)
+                    + (keysState['ArrowDown'] ? 50 : 0)
+                )
+
+                this.xVel = (
+                    (keysState['ArrowLeft'] ? -50 : 0)
+                    + (keysState['ArrowRight'] ? 50 : 0)
+                )
+            },
+
+            update: function (deltaTime) {
+                this.x += this.xVel * (deltaTime)
+                this.y += this.yVel * (deltaTime)
+
+                this.animation.update(deltaTime)
+            },
+
+            render (ctx, deltaTime) {
+                this.spriteSheet.frames['walk-down-1'].draw(ctx, this.x, this.y)
+            }
+        },
+
+        main = () => {
+            const keysState = {}
+
+            ;['keydown', 'keyup'].forEach((eventName) => {
+                document.addEventListener(eventName, (e) => {
+                    e.preventDefault()
+                    keysState[e.key] = eventName == 'keydown' ? true : false
+                    player.handleInput(e, keysState)
+                })
+            })
+
+            let
+                lastTime = Date.now() / 1000,
+                deltaTime = 0
+
+            const loop = () => {
+                update(deltaTime)
+                render(ctx, deltaTime)
+
+                now = Date.now() / 1000
+                deltaTime = now - lastTime
+                lastTime = now
+            }
+
+            setInterval(loop, 1000 / 60)
+        },
+
+        update = (deltaTime) => {
+            player.update(deltaTime)
+        },
+
+        render = (deltaTime) => {
+            // get draw params
+
+            // draw them
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            player.render(ctx)
+        }
+
+
+    main()
+})()
