aeb12109c41ffc979983fe60156f46ed91baee72
kugurerdem <k.ugurerdem@gmail.com>
2023-12-08 19:15:25 +0300

    Seperate assets from index module

---
+++ assets.js
@@ -0,0 +1,93 @@
+const assets = (() => {
+    const
+        {fromPairs, range} = _, // eslint disable no-undef
+        {SpriteSheet, loadImage} = utils,
+
+        playerSpriteSheet = loadImage('assets/characters/player.png').then(
+            image => new SpriteSheet({
+                image,
+
+                frames: fromPairs(range(0, 6).flatMap((i) => ([
+                    [`idle-down-${i}`, {x: i * 48, y: 0, w: 48, h: 48}],
+                    [`idle-right-${i}`, {x: i * 48, y: 48, w: 48, h: 48}],
+                    [`idle-left-${i}`, {
+                        x: i * 48, y: 48, w: 48, h: 48, flip: true,
+                    }],
+                    [`idle-up-${i}`, {x: i * 48, y: 2 * 48, w: 48, h: 48}],
+
+                    [`walk-right-${i}`, {x: i * 48, y: 4 * 48, w: 48, h: 48}],
+                    [`walk-left-${i}`, {
+                        x: i * 48, y: 4 * 48, w: 48, h: 48, flip: true,
+                    }],
+                    [`walk-up-${i}`, {x: i * 48, y: 5 * 48, w: 48, h: 48}],
+                    [`walk-down-${i}`, {x: i * 48, y: 3 * 48, w: 48, h: 48}],
+
+                    [`attack-down-${i}`, {x: i * 48, y: 6 * 48, w: 48, h: 48}],
+                    [`attack-right-${i}`, {x: i * 48, y: 7 * 48, w: 48, h: 48}],
+                    [`attack-left-${i}`, {
+                        x: i * 48, y: 7 * 48, w: 48, h: 48, flip: true,
+                    }],
+                    [`attack-up-${i}`, {x: i * 48, y: 8 * 48, w: 48, h: 48}],
+                ]))),
+
+                animations: {
+                    'idle-down': {
+                        frames: range(0,6).map((i) => `idle-down-${i}`),
+                        frameRate: 6,
+                    },
+                    'idle-right': {
+                        frames: range(0,6).map((i) => `idle-right-${i}`),
+                        frameRate: 6,
+                    },
+                    'idle-left': {
+                        frames: range(0,6).map((i) => `idle-left-${i}`),
+                        frameRate: 6,
+                    },
+                    'idle-up': {
+                        frames: range(0,6).map((i) => `idle-up-${i}`),
+                        frameRate: 6,
+                    },
+
+
+                    'walk-right': {
+                        frames: range(0,6).map((i) => `walk-right-${i}`),
+                        frameRate: 6,
+                    },
+                    'walk-up': {
+                        frames: range(0,6).map((i) => `walk-up-${i}`),
+                        frameRate: 6,
+                    },
+                    'walk-left': {
+                        frames: range(0,6).map((i) => `walk-left-${i}`),
+                        frameRate: 6,
+                    },
+                    'walk-down': {
+                        frames: range(0,6).map((i) => `walk-down-${i}`),
+                        frameRate: 6,
+                    },
+
+
+                    'attack-down': {
+                        frames: range(0,4).map((i) => `attack-down-${i}`),
+                        frameRate: 8,
+                    },
+                    'attack-right': {
+                        frames: range(0,4).map((i) => `attack-right-${i}`),
+                        frameRate: 8,
+                    },
+                    'attack-left': {
+                        frames: range(0,4).map((i) => `attack-left-${i}`),
+                        frameRate: 8,
+                    },
+                    'attack-up': {
+                        frames: range(0,4).map((i) => `attack-up-${i}`),
+                        frameRate: 8,
+                    },
+                },
+
+                scale: 2,
+            }),
+        )
+
+    return {playerSpriteSheet}
+})()
--- index.js
+++ index.js
@@ -3,95 +3,14 @@
     const
         {fromPairs, invert, range, flatMap} = _,
         {SpriteSheet, loadImage} = utils,
+        {playerSpriteSheet} = assets,
 
         canvas = document.getElementById('canvas'),
-        ctx = canvas.getContext('2d'),
+        ctx = canvas.getContext('2d')
 
-        spriteSheet = await loadImage('assets/characters/player.png').then((sheetImg) => {
-            return new SpriteSheet({
-                image: sheetImg,
-                frames: fromPairs(range(0, 6).flatMap((i) => ([
-                    [`idle-down-${i}`, {x: i * 48, y: 0, w: 48, h: 48}],
-                    [`idle-right-${i}`, {x: i * 48, y: 48, w: 48, h: 48}],
-                    [`idle-left-${i}`, {
-                        x: i * 48, y: 48, w: 48, h: 48, flip: true,
-                    }],
-                    [`idle-up-${i}`, {x: i * 48, y: 2 * 48, w: 48, h: 48}],
+    spriteSheet = await playerSpriteSheet
 
-                    [`walk-right-${i}`, {x: i * 48, y: 4 * 48, w: 48, h: 48}],
-                    [`walk-left-${i}`, {
-                        x: i * 48, y: 4 * 48, w: 48, h: 48, flip: true,
-                    }],
-                    [`walk-up-${i}`, {x: i * 48, y: 5 * 48, w: 48, h: 48}],
-                    [`walk-down-${i}`, {x: i * 48, y: 3 * 48, w: 48, h: 48}],
-
-                    [`attack-down-${i}`, {x: i * 48, y: 6 * 48, w: 48, h: 48}],
-                    [`attack-right-${i}`, {x: i * 48, y: 7 * 48, w: 48, h: 48}],
-                    [`attack-left-${i}`, {
-                        x: i * 48, y: 7 * 48, w: 48, h: 48, flip: true,
-                    }],
-                    [`attack-up-${i}`, {x: i * 48, y: 8 * 48, w: 48, h: 48}],
-                ]))),
-
-                animations: {
-                    'idle-down': {
-                        frames: range(0,6).map((i) => `idle-down-${i}`),
-                        frameRate: 6,
-                    },
-                    'idle-right': {
-                        frames: range(0,6).map((i) => `idle-right-${i}`),
-                        frameRate: 6,
-                    },
-                    'idle-left': {
-                        frames: range(0,6).map((i) => `idle-left-${i}`),
-                        frameRate: 6,
-                    },
-                    'idle-up': {
-                        frames: range(0,6).map((i) => `idle-up-${i}`),
-                        frameRate: 6,
-                    },
-
-
-                    'walk-right': {
-                        frames: range(0,6).map((i) => `walk-right-${i}`),
-                        frameRate: 6,
-                    },
-                    'walk-up': {
-                        frames: range(0,6).map((i) => `walk-up-${i}`),
-                        frameRate: 6,
-                    },
-                    'walk-left': {
-                        frames: range(0,6).map((i) => `walk-left-${i}`),
-                        frameRate: 6,
-                    },
-                    'walk-down': {
-                        frames: range(0,6).map((i) => `walk-down-${i}`),
-                        frameRate: 6,
-                    },
-
-
-                    'attack-down': {
-                        frames: range(0,4).map((i) => `attack-down-${i}`),
-                        frameRate: 8,
-                    },
-                    'attack-right': {
-                        frames: range(0,4).map((i) => `attack-right-${i}`),
-                        frameRate: 8,
-                    },
-                    'attack-left': {
-                        frames: range(0,4).map((i) => `attack-left-${i}`),
-                        frameRate: 8,
-                    },
-                    'attack-up': {
-                        frames: range(0,4).map((i) => `attack-up-${i}`),
-                        frameRate: 8,
-                    },
-                },
-
-                scale: 2,
-            })
-        }),
-
+    const
         player = {
             spriteSheet,
 
